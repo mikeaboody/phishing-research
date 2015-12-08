64bb2398cdfcdbbe1d73b1604415cb2110f837f0
Matthew Chow <mchow@twitter.com>
2015-12-08 05:11:27 -0800

    Initial commit of feature generating script

---
+++ common/feature_classes.py
@@ -0,0 +1,108 @@
+import mailbox
+
+from detector import Detector
+
+class MessageIdDetectorOne(Detector):
+    DELIMITERS = ['.', '-', '$', '/', '%']
+
+    def __init__(self, regular_mbox):
+        self.inbox = regular_mbox
+
+    def most_common_delimiter(self, uid):
+        highest_count = 0
+        for delimiter in self.DELIMITERS:
+            delim_count = uid.count(delimiter)
+            if delim_count > highest_count:
+                highest_count = delim_count
+                freq_delim = delimiter
+        if highest_count == 0:
+            return None, 0
+        else:
+            return freq_delim, highest_count
+
+    def partition_within_error(self, features, new_feature, fudge_factor=0):
+        for old_feature in features:
+            if old_feature[0] != new_feature[0] or old_feature[1] != new_feature[1]:
+                continue
+            old_partition_sizes = old_feature[2].split(' ')
+            new_partition_sizes = new_feature[2].split(' ')
+            old_sum = sum([int(x) for x in old_partition_sizes])
+            new_sum = sum([int(x) for x in new_partition_sizes])
+            if abs(old_sum - new_sum) <= fudge_factor:
+                return True
+        return False
+
+    def partition_length(self, uid, delimiter, count):
+        if count == 0:
+            return str(len(uid))
+        split_uid = uid.split(delimiter)
+        return " ".join([str(len(split)) for split in split_uid])
+
+    def create_sender_profile(self, num_samples):
+        self.sender_profile = {}
+        for i in range(num_samples):
+            msg = self.inbox[i]
+            sender = self.extract_from(msg)
+            message_id = msg["Message-ID"]
+            split_msg_id = message_id.split('@')
+            if len(split_msg_id) < 2:
+                print("Message-ID misformatted: {}".format(message_id))
+                continue
+            domain = split_msg_id[1][:-1]
+            uid = split_msg_id[0][1:]
+            common_delimiter, delimiter_count = self.most_common_delimiter(uid)
+            partition_sizes = self.partition_length(uid, common_delimiter, delimiter_count)
+            features = (common_delimiter, delimiter_count, partition_sizes)
+            if sender in self.sender_profile:
+                is_valid = self.partition_within_error(self.sender_profile[sender], features, fudge_factor=2)
+                if not is_valid:
+                    self.sender_profile[sender].add(features)
+            else:
+                self.sender_profile[sender] = set([features])
+        return self.sender_profile
+
+    def classify(self, phish):
+        sender = self.extract_from(phish)
+        message_id = phish["Message-ID"]
+        split_msg_id = message_id.split('@')
+        if len(split_msg_id) < 2:
+            print("Message-ID misformatted: {}".format(message_id))
+            return False
+        domain = split_msg_id[1][:-1]
+        uid = split_msg_id[0][1:]
+        common_delimiter, delimiter_count = self.most_common_delimiter(uid)
+        partition_sizes = self.partition_length(uid, common_delimiter, delimiter_count)
+        features = (common_delimiter, delimiter_count, partition_sizes)
+        if sender in self.sender_profile:
+            is_valid = self.partition_within_error(self.sender_profile[sender], features, fudge_factor=0)
+            return not is_valid
+        else:
+            return False
+
+    def modify_phish(self, phish, msg):
+        phish['Message-ID'] = msg['Message-ID']
+        return phish
+
+class MessageIdDetectorTwo(MessageIdDetectorOne):
+    def create_sender_profile(self, num_samples):
+        self.sender_profile = {}
+        for i in range(num_samples):
+            msg = self.inbox[i]
+            sender = self.extract_from(msg)
+            message_id = msg["Message-ID"]
+            split_msg_id = message_id.split('@')
+            if len(split_msg_id) < 2:
+                print("Message-ID misformatted: {}".format(message_id))
+                continue
+            domain = split_msg_id[1][:-1]
+            uid = split_msg_id[0][1:]
+            common_delimiter, delimiter_count = self.most_common_delimiter(uid)
+            partition_sizes = self.partition_length(uid, common_delimiter, delimiter_count)
+            features = (common_delimiter, delimiter_count, partition_sizes)
+            if sender in self.sender_profile:
+                is_valid = self.partition_within_error(self.sender_profile[sender], features, fudge_factor=2)
+                if not is_valid:
+                    self.sender_profile[sender].add(features)
+            else:
+                self.sender_profile[sender] = set([features])
+        return self.sender_profile
---
+++ common/generate_features.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+""" 
+generate_features.py: Script that reads in spear-phishing and legitimate emails
+and converts each training example into a feature vector.
+
+Requirements:
+-numpy ('pip install numpy')
+-scipy ('pip install scipy')
+
+The output of your file will be a .mat file. The data will be accessible using
+the following keys:
+    -'training_data'
+    -'training_labels'
+"""
+
+import mailbox
+import numpy as np
+import scipy.io as sio
+
+import feature_classes as fc
+
+# PHISHING_FILENAME = 'phish.mbox'
+PHISHING_FILENAME = 'matthew_berkeley.mbox'
+# REGULAR_FILENAME = 'regular.mbox'
+REGULAR_FILENAME = 'Inbox.mbox'
+NUM_PRE_TRAINING = 100
+
+############
+# FEATURES #
+############
+"""
+Define your features in feature_classes.py, and add the class name to the
+features list below. Feature classes must inherit from the Detector class
+defined in detector.py.
+"""
+
+features = [
+    fc.MessageIdDetectorOne,
+    fc.MessageIdDetectorTwo
+]
+
+def generate_data_matrix(phishing_mbox, regular_mbox):
+    assert len(regular_mbox) > NUM_PRE_TRAINING, "Not enough data points."
+    num_data_points = len(phishing_mbox) + len(regular_mbox) - NUM_PRE_TRAINING
+    num_features = len(features)
+    data_matrix = np.empty(shape=(num_data_points, num_features))
+    detectors = [Detector(regular_mbox) for Detector in features]
+    print("Building sender profiles for each feature...")
+    for detector in detectors:
+        detector.create_sender_profile(NUM_PRE_TRAINING)
+    print("Generating data matrix...")
+    row_index = 0
+    for i in range(NUM_PRE_TRAINING, len(regular_mbox)):
+        for j, detector in enumerate(detectors):
+            data_matrix[row_index][j] = 1 if detector.classify(regular_mbox[i]) else 0
+        row_index += 1
+    for i in range(len(phishing_mbox)):
+        for j, detector in enumerate(detectors):
+            data_matrix[row_index][j] = 1 if detector.classify(phishing_mbox[i]) else 0
+        row_index += 1
+    return data_matrix
+
+def generate_labels(phishing_mbox, regular_mbox):
+    assert len(regular_mbox) > NUM_PRE_TRAINING, "Not enough data points."
+    num_data_points = len(phishing_mbox) + len(regular_mbox) - NUM_PRE_TRAINING
+    label_matrix = np.empty(shape=(num_data_points, 1))
+    print("Generating label matrix...")
+    for i in range(num_data_points):
+        label_matrix[i][0] = 0 if i < len(regular_mbox) - NUM_PRE_TRAINING else 1
+    return label_matrix
+
+
+######################
+# Script Starts Here #
+######################
+
+phishing_emails = mailbox.mbox(PHISHING_FILENAME)
+regular_emails = mailbox.mbox(REGULAR_FILENAME)
+X = generate_data_matrix(phishing_emails, regular_emails)
+Y = generate_labels(phishing_emails, regular_emails)
+
+file_dict = {}
+file_dict['training_data'] = X
+file_dict['training_labels'] = Y
+sio.savemat('phishing_data.mat', file_dict)
+
+print("Data matrix has been generated. There are {} total training points and {} total features.".format(len(X), len(X[0])))
