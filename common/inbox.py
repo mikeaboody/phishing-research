c3d2f01748c9d68da6b5d4975b4490c14ea7f9a8
Mike Aboody <mikeaboody@berkeley.edu>
2016-03-31 15:50:04 -0700

    Edited inbox class to be simpler and added get_all method for emails

--- common/inbox.py
+++ common/inbox.py
@@ -7,28 +7,30 @@ class Inbox():
 		self.processEmails(root)
 	def processEmails(self, root):
 		for item in os.listdir(root):
-			level1Dir = os.path.join(root, item)
-			if os.path.isdir(level1Dir):
-				for item in os.listdir(level1Dir):
-					level2Dir = os.path.join(level1Dir, item)
-					if os.path.isdir(level2Dir):
-						for item in os.listdir(level2Dir):
-							level3Dir = os.path.join(level2Dir, item)
-							if os.path.isdir(level3Dir):
-								for item in os.listdir(level3Dir):
-									logFileName = os.path.join(level3Dir, item)
-									if os.path.isfile(logFileName) and logFileName.endswith(".log"):
-										#read log file
-										with open(logFileName, "r") as logFile:
-											for line in logFile:
-												currEmailDict = eval(line)
-												currEmailDict = defaultdict(lambda: None, currEmailDict)
-												self.emails.append(currEmailDict)
+			subDir = os.path.join(root, item)
+			if os.path.isdir(subDir):
+				self.processEmails(subDir)
+			else:
+				logFileName = os.path.join(root, item)
+				if os.path.isfile(logFileName) and logFileName.endswith(".log"):
+					with open(logFileName, "r") as logFile:
+						for line in logFile:
+							currEmailDict = eval(line)
+							self.emails.append(Email(currEmailDict))										
 	def __getitem__(self, key):
-		return self.emails[key.upper()]
+		return self.emails[key]
 	def __setitem__(self, key, value):
-		self.emails[key.upper()] = value
+		self.emails[key] = value
 	def __len__(self):
 		return len(self.emails)
 	def __iter__(self):
 		return iter(self.emails)
+class Email():
+	def __init__(self, emailDict):
+		self.headers = defaultdict(lambda: None, emailDict)
+	def __getitem__(self, key):
+		return self.headers[key.upper()]
+	def __setitem__(self, key, value):
+		self.emails[key.upper()] = value
+	def get_all(self, key):
+		return self[key]
