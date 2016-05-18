1bac9bc29b0702ae0b78249cdd2add746e11441b
nexusapoorvacus <nexusapoorvacus19@gmail.com>
2016-05-18 10:57:47 -0700

    removed slashes from folder names

--- broScripts/parse_pcap.py
+++ broScripts/parse_pcap.py
@@ -99,13 +99,13 @@ try:
                             break
                     if not is_person_empty(sender):
                         name, address = parseaddr(sender)
-                        name = name[:20]
-                        address = address[:50]
+                        name = name[:20].replace("/","")
+                        address = address[:50].replace("/","")
                     else:
                         name, address = '', ''
                     if name:
-                        first_subdir = name[:3]
-                        second_subdir = name[3:6]
+                        first_subdir = name[:3].replace("/", "")
+                        second_subdir = name[3:6].replace("/", "")
                         if second_subdir == '':
                             second_subdir = 'none'
                         sender_dir = "{}/{}/{}/{}/{}".format(OUTPUT_DIRECTORY,
