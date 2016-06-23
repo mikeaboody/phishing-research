2d204316e50c6a4ca66529b4d9b0d43003138585
Mike Aboody <mikeaboody@berkeley.edu>
2016-06-22 23:40:48 -0700

    Created memory tracking tool on guppy

---
+++ common/memtest.py
@@ -0,0 +1,31 @@
+import os
+from guppy import hpy
+
+class MemTracker(object):
+    log_file_path = None
+    heapy_instance = None
+
+    @classmethod
+    def initialize(cls, file_path):
+        if MemTracker.log_file_path != None:
+            raise RuntimeError("Already initialized.")
+        MemTracker.log_file_path = file_path
+        if os.path.exists(MemTracker.log_file_path):
+            os.remove(MemTracker.log_file_path)
+        with open(MemTracker.log_file_path, "a") as f:
+            f.write("Beggining memory tracking.\n\n")
+        MemTracker.heapy_instance = hpy()
+        MemTracker.heapy_instance.setrelheap()
+
+
+    @classmethod
+    def logMemory(cls, section_name):
+        if MemTracker.log_file_path == None:
+            raise RuntimeError("Initialize before calling.")
+        
+        h = MemTracker.heapy_instance.heap()
+        with open(MemTracker.log_file_path, "a") as f:
+            f.write("Memory statistics for '" + section_name + "':\n")
+            for i in range(len(h)):
+                f.write(str(h[i]) + "\n")
+            f.write("\n")
