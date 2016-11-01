517d70644daec1c4d0df7f17e046e5d4486bcb18
nexusapoorvacus <nexusapoorvacus19@gmail.com>
2016-11-01 13:46:51 -0700

    added arguments to mboxToFile and cleaned parse_pcap.py

--- broScripts/mboxToFile.py
+++ broScripts/mboxToFile.py
@@ -1,12 +1,11 @@
 import sys
 import mailbox
+import progressbar
 
-def createFile(inbox):
-    i = 0
-    for msg in inbox:
-        if i >= 50:
-            break
-        print(i)
+def createFile(inbox, num_emails):
+    progress = progressbar.ProgressBar()
+    for i in progress(range(num_emails)):
+        msg = inbox[i]
         line = "["
         for header, value in msg.items():
             value = value.replace("\r", "")
@@ -21,9 +20,18 @@ def createFile(inbox):
         outputFile.write(line + "\n")
         i += 1
 
-
-
-mboxFile = "/home/apoorva/Documents/Research/PhishingResearch/Inbox.mbox"
-outputFile = open("emailFile.log", "w")
+if len(sys.argv) < 2:
+    print("Please specify the absolute path to the mbox as an argument")
+    sys.exit()
+mboxFile = sys.argv[1]
+outputFile = open("mbox.log", "w")
 inbox = mailbox.mbox(mboxFile)
-createFile(inbox)
+inbox_length = len(inbox)
+num_emails = inbox_length
+if len(sys.argv) == 3:
+    try:
+        # the number of emails passed in must be less than the total number of emails in the mbox
+        num_emails = min(int(sys.argv[2]), inbox_length)
+    except:
+        print("The second (optional) argument should be a number")
+createFile(inbox, num_emails)
--- broScripts/parse_pcap.py
+++ broScripts/parse_pcap.py
@@ -12,13 +12,10 @@ FILE_PATH = os.path.realpath(__file__)
 DIR_NAME = os.path.dirname(FILE_PATH)
 
 CURR_DIR = os.getcwd()
-
-PCAP_DIRECTORY = DIR_NAME + '/input'
+MBOX_FILE_NAME = "mbox.log"
+MBOX_FILE = "broScripts/mbox.log"
 OUTPUT_DIRECTORY = 'broScripts/output'
 BRO_LOG_DIRECTORY = DIR_NAME + '/bro_logs'
-
-BRO_SCRIPT_PATH = DIR_NAME + '/main.bro'
-BRO_OUTPUT_FILE = CURR_DIR + '/smtp.log'
 
 SENDERS_FILE = OUTPUT_DIRECTORY + '/senders.log'
 
@@ -26,11 +23,9 @@ total_senders = 0
 total_legit_emails = 0
 total_phish_emails = 0
 total_failed_parse = 0
-total_pcaps = 0
 total_only_hyphen = 0
 total_full_parse_error = 0
 total_quote_paren_error = 0
-num_pcaps_bro_failed = 0
 
 eval_error_count = 0
 mkdir_error_count = 0
@@ -51,8 +46,6 @@ def summary_stats():
     progress_logger.info("Number of emails successfully parsed: {}".format(total_legit_emails))
     progress_logger.info("Number of pseudo-phish emails generated: {}".format(total_phish_emails))
     progress_logger.info("Number of emails failed to parse: {}".format(total_failed_parse))
-    progress_logger.info("Number of pcaps parsed: {}".format(total_pcaps))
-    progress_logger.info("Number of pcaps that Bro failed to parse: {}".format(num_pcaps_bro_failed))
     progress_logger.info("Number of emails represented only by a hyphen: {}".format(total_only_hyphen))
     progress_logger.info("Number of emails that could not be parsed fully: {}".format(total_full_parse_error))
     progress_logger.info("Number of emails with a quote+parenthesis in the header or header value: {}".format(total_quote_paren_error))
@@ -109,70 +102,59 @@ try:
     # Generating legit emails
     senders_seen = open(SENDERS_FILE, 'a+')
     dir_num = 0
-    #for filename in glob.glob(PCAP_DIRECTORY + '/*.pcap'):
-    for filename in ["file"]:
-        #try:
-        #    call(['bro', '-r', filename, '-b', BRO_SCRIPT_PATH])
-        #except Exception as e:
-        #    debug_logger.warn('Could not invoke bro on {}'.format(filename))
-        #    num_pcaps_bro_failed += 1
-        #    continue
-        with open(BRO_OUTPUT_FILE, 'a+') as f:
-            for line in f:
-                if line == "-" or line == "-\n":
-                    total_only_hyphen += 1
+    with open(MBOX_FILE, 'a+') as f:
+        for line in f:
+            if line == "-" or line == "-\n":
+                total_only_hyphen += 1
+                continue
+            if line[0] == '[' and line[-2] == ']': # Check that this line represents an email
+                try:
+                    headers = parseLine(line)
+                except (SyntaxError, ValueError) as e:
+                    if eval_error_count < 10:
+                        eval_error_count += 1
+                        debug_logger.exception(e)
+                    total_failed_parse += 1
                     continue
-                if line[0] == '[' and line[-2] == ']': # Check that this line represents an email
+                sender = ''
+                for k, v in headers:
+                    if k == 'FROM':
+                        sender = v
+                        senders_seen.write(v + '\n')
+                        break
+                if not is_person_empty(sender):
+                    name, address = parseaddr(sender)
+                    name = name[:20].replace("/","")
+                    address = address[:50].replace("/","")
+                else:
+                    name, address = '', ''
+                if name:
+                    first_subdir = name[:3].replace("/", "")
+                    second_subdir = name[3:6].replace("/", "")
+                    if second_subdir == '':
+                        second_subdir = 'none'
+                    sender_dir = "{}/{}/{}/{}/{}".format(OUTPUT_DIRECTORY,
+                        first_subdir, second_subdir, name, address)
+                else:
+                    name = 'noname'
+                    sender_dir = "{}/{}/{}".format(OUTPUT_DIRECTORY, name, address)
+                if not os.path.exists(sender_dir):
+                    dir_num += 1
+                    progress_logger.info('Creating Directory #{}'.format(dir_num))
                     try:
-                        headers = parseLine(line)
-                    except (SyntaxError, ValueError) as e:
-                        if eval_error_count < 10:
-                            eval_error_count += 1
+                        os.makedirs(sender_dir)
+                        total_senders += 1
+                    except Exception as e:
+                        if mkdir_error_count < 10:
+                            mkdir_error_count += 1
                             debug_logger.exception(e)
-                        total_failed_parse += 1
-                        continue
-                    sender = ''
-                    for k, v in headers:
-                        if k == 'FROM':
-                            sender = v
-                            senders_seen.write(v + '\n')
-                            break
-                    if not is_person_empty(sender):
-                        name, address = parseaddr(sender)
-                        name = name[:20].replace("/","")
-                        address = address[:50].replace("/","")
-                    else:
-                        name, address = '', ''
-                    if name:
-                        first_subdir = name[:3].replace("/", "")
-                        second_subdir = name[3:6].replace("/", "")
-                        if second_subdir == '':
-                            second_subdir = 'none'
-                        sender_dir = "{}/{}/{}/{}/{}".format(OUTPUT_DIRECTORY,
-                            first_subdir, second_subdir, name, address)
-                    else:
-                        name = 'noname'
-                        sender_dir = "{}/{}/{}".format(OUTPUT_DIRECTORY, name, address)
-                    if not os.path.exists(sender_dir):
-                        dir_num += 1
-                        progress_logger.info('Creating Directory #{}'.format(dir_num))
-                        try:
-                            os.makedirs(sender_dir)
-                            total_senders += 1
-                        except Exception as e:
-                            if mkdir_error_count < 10:
-                                mkdir_error_count += 1
-                                debug_logger.exception(e)
-                    with open('{}/legit_emails.log'.format(sender_dir), 'a') as output_file:
-                        output_file.write(line)
-                        total_legit_emails += 1
-        last_index = filename.rfind('/')
-        bro_filename = filename[last_index + 1:-4] + 'log'
-        try:
-            call(['cp', BRO_OUTPUT_FILE, '{}/{}'.format(BRO_LOG_DIRECTORY, "mbox.log")])
-        except Exception as e:
-            debug_logger.exception("Unable to move {}".format(bro_filename))
-        total_pcaps += 1
+                with open('{}/legit_emails.log'.format(sender_dir), 'a') as output_file:
+                    output_file.write(line)
+                    total_legit_emails += 1
+    try:
+        call(['cp', MBOX_FILE, '{}/{}'.format(BRO_LOG_DIRECTORY, MBOX_FILE_NAME)])
+    except Exception as e:
+        debug_logger.exception("Unable to move {}".format(bro_filename))
     progress_logger.info('Done.  Created #{} directories.'.format(dir_num))
     senders_seen.close()
 
@@ -187,8 +169,6 @@ try:
                     try:
                         headers = parseLine(line)
                     except (SyntaxError, ValueError) as e:
-                        # total_failed_parse += 1
-                        # print(e)
                         continue
                     sender = ''
                     for k, v in headers:
